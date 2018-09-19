04d48bab84f200c7bb890614331b3997f29e7e0e
Sabrina Moraru <sabrina.moraru@gmail.com>
2018-09-19 19:11:28 +0300

    removing a note

--- notes.js
+++ notes.js
@@ -39,7 +39,14 @@ var getNote = title => {
 };
 
 var removeNote = title => {
-  console.log("Removing note", title);
+  // fetch notes
+  var notes = fetchNotes();
+  // filter notes, removing the one with title of argument
+  var filteredNotes = notes.filter(note => note.title !== title);
+  // save new notes array
+  saveNotes(filteredNotes);
+
+  return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
